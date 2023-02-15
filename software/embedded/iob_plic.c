9cb06b206c848d0d12eaa079d14c6a8df5a965ea
Pedro Antunes <pedronmantunes@gmail.com>
2023-02-15 01:51:52 +0000

    Firmware: Fix code errors;

--- software/embedded/iob_plic.c
+++ software/embedded/iob_plic.c
@@ -6,19 +6,20 @@
 void plic_init(int base_address){
     base = base_address;
 
+    int i = 0;
     //clear all EL
     for (i=0; i < EDGE_LEVEL_REGS; i++)
-        err = plic_write((EL_BASE_ADDRESS+i)*DATA_W/8, 0);
+        plic_write((EL_BASE_ADDRESS+i)*DATA_W/8, 0);
     //set priority for all sources to '1'; '0' means 'never interrupt'
     int write_pr = write_pr_regs();
     for (i=0; i < PRIORITY_REGS; i++)
-        err = plic_write((PR_BASE_ADDRESS+i)*DATA_W/8, write_pr);
+        plic_write((PR_BASE_ADDRESS+i)*DATA_W/8, write_pr);
     //clear all IE
     for (i=0; i < IE_REGS; i++)
-        err = plic_write((IE_BASE_ADDRESS+i)*DATA_W/8, 0);
+        plic_write((IE_BASE_ADDRESS+i)*DATA_W/8, 0);
     //set all threshold to '0'
     for (i=0; i < PTHRESHOLD_REGS; i++)
-        err = plic_write((TH_BASE_ADDRESS+i)*DATA_W/8, 0);
+        plic_write((TH_BASE_ADDRESS+i)*DATA_W/8, 0);
 }
 void plic_write(int address, int data){
     (*(volatile uint32_t *) (base+address))     = (uint32_t)(data);
@@ -38,7 +39,7 @@ void plic_disable_interrupt(int source){
     plic_write((IE_BASE_ADDRESS+(target*EDGE_LEVEL_REGS)+(source/DATA_W))*DATA_W/8, 0);
 }
 /* Returns interrupt ID */
-void plic_claim_interrupt(){
+int plic_claim_interrupt(){
     int target;
     target = csr_read_mhartid();
     return plic_read((ID_BASE_ADDRESS + target) * DATA_W/8);
@@ -47,7 +48,7 @@ void plic_claim_interrupt(){
 void plic_complete_interrupt(int source_id){
     int target;
     target = csr_read_mhartid();
-    plic_write((id_base_address + target) * DATA_W/8, 1 << (source_id % DATA_W));
+    plic_write((ID_BASE_ADDRESS + target) * DATA_W/8, 1 << (source_id % DATA_W));
 }
 
 int write_pr_regs(){
