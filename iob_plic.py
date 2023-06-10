3538d0b9385bafce1e439ac09106b9c9887e7655
Artur Nóbrega <arturnobrega1999@gmail.com>
2023-06-10 19:19:56 +0100

    feat(python): Add class for object-based setup

---
+++ iob_plic.py
@@ -0,0 +1,195 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+
+from iob_module import iob_module
+from setup import setup
+
+# Submodules
+from iob_lib import iob_lib
+from iob_utils import iob_utils
+from iob_clkenrst_portmap import iob_clkenrst_portmap
+from iob_clkenrst_port import iob_clkenrst_port
+from iob_reg_re import iob_reg_re
+
+class iob_plic(iob_module):
+    name = "iob_plic"
+    version = "V0.10"
+    flows = "sim emb"
+    setup_dir=os.path.dirname(__file__)
+
+    @classmethod
+    def _run_setup(cls):
+        # Hardware headers & modules
+        iob_module.generate("iob_s_port")
+        iob_module.generate("iob_s_portmap")
+        iob_module.generate("iob_wire")
+        iob_lib.setup()
+        iob_utils.setup()
+        iob_clkenrst_portmap.setup()
+        iob_clkenrst_port.setup()
+        iob_reg_re.setup()
+
+        cls._setup_confs()
+        cls._setup_ios()
+        cls._setup_regs()
+        cls._setup_block_groups()
+
+        # Verilog modules instances
+        # TODO
+
+        # Copy sources of this module to the build directory
+        super()._run_setup()
+
+        # Setup core using LIB function
+        setup(cls)
+
+
+    @classmethod
+    def _setup_confs(cls):
+        super()._setup_confs([
+            # Macros
+
+            # Parameters
+            # IOb-bus Parameters
+            {
+                "name": "DATA_W",
+                "type": "P",
+                "val": "32",
+                "min": "NA",
+                "max": "NA",
+                "descr": "Data bus width",
+            },
+            {
+                "name": "ADDR_W",
+                "type": "P",
+                "val": "16",
+                "min": "NA",
+                "max": "NA",
+                "descr": "Address bus width",
+            },
+            # PLIC Parameters
+            {
+                "name": "N_SOURCES",
+                "type": "P",
+                "val": "64",
+                "min": "NA",
+                "max": "64",
+                "descr": "Number of interrupt sources",
+            },
+            {
+                "name": "N_TARGETS",
+                "type": "P",
+                "val": "4",
+                "min": "NA",
+                "max": "8",
+                "descr": "Number of interrupt targets",
+            },
+            {
+                "name": "PRIORITIES",
+                "type": "P",
+                "val": "8",
+                "min": "NA",
+                "max": "8",
+                "descr": "Number of Priority levels",
+            },
+            {
+                "name": "MAX_PENDING_COUNT",
+                "type": "P",
+                "val": "8",
+                "min": "NA",
+                "max": "8",
+                "descr": "Max. number of 'pending' events",
+            },
+            {
+                "name": "HAS_THRESHOLD",
+                "type": "P",
+                "val": "1",
+                "min": "NA",
+                "max": "1",
+                "descr": "Is 'threshold' implemented?",
+            },
+            {
+                "name": "HAS_CONFIG_REG",
+                "type": "P",
+                "val": "1",
+                "min": "NA",
+                "max": "1",
+                "descr": "Is the 'configuration' register implemented?",
+            },
+        ])
+
+    @classmethod
+    def _setup_ios(cls):
+        cls.ios += [
+            {"name": "iob_s_port", "descr": "CPU native interface", "ports": []},
+            {
+                "name": "general",
+                "descr": "GENERAL INTERFACE SIGNALS",
+                "ports": [
+                    {
+                        "name": "clk_i",
+                        "type": "I",
+                        "n_bits": "1",
+                        "descr": "System clock input",
+                    },
+                    {
+                        "name": "arst_i",
+                        "type": "I",
+                        "n_bits": "1",
+                        "descr": "System reset, asynchronous and active high",
+                    },
+                    {
+                        "name": "cke_i",
+                        "type": "I",
+                        "n_bits": "1",
+                        "descr": "System clock enable",
+                    },
+                ],
+            },
+            {
+                "name": "plic_io",
+                "descr": "PLIC specific IO.",
+                "ports": [
+                    # {'name':'interrupt', 'type':'O', 'n_bits':'1', 'descr':'be done'},
+                    {
+                        "name": "src",
+                        "type": "I",
+                        "n_bits": "N_SOURCES",
+                        "descr": "Interrupt sources",
+                    },
+                    {
+                        "name": "irq",
+                        "type": "O",
+                        "n_bits": "N_TARGETS",
+                        "descr": "Interrupt Requests",
+                    },
+                ],
+            },
+        ]
+
+    @classmethod
+    def _setup_regs(cls):
+        cls.regs += [
+            {
+                "name": "dummy",
+                "descr": "Dummy registers to run register setup functions",
+                "regs": [
+                    {
+                        "name": "DUMMY",
+                        "type": "R",
+                        "n_bits": 1,
+                        "rst_val": 0,
+                        "addr": -1,
+                        "log2n_items": 0,
+                        "autologic": False,
+                        "descr": "Dummy Register",
+                    },
+                ],
+            }
+        ]
+
+    @classmethod
+    def _setup_block_groups(cls):
+        cls.block_groups += []
