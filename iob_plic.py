caaae90f3083ad80a225298c7a60f2c35fa6e75b
Artur Nóbrega <arturnobrega1999@gmail.com>
2026-02-10 18:58:42 +0000

    feat(py2): Update PLIC to support latest version of Py2HWSW.

--- iob_plic.py
+++ iob_plic.py
@@ -1,180 +1,287 @@
-#!/usr/bin/env python3
-
-import os
-
-from iob_module import iob_module
-
-# Submodules
-from iob_utils import iob_utils
-from iob_reg_re import iob_reg_re
+# SPDX-FileCopyrightText: 2025 IObundle
+#
+# SPDX-License-Identifier: MIT
 
 
-class iob_plic(iob_module):
-    name = "iob_plic"
-    version = "V0.10"
-    flows = "sim emb"
-    setup_dir = os.path.dirname(__file__)
+def setup(py_params_dict):
+    CSR_IF = py_params_dict["csr_if"] if "csr_if" in py_params_dict else "iob"
 
-    @classmethod
-    def _create_submodules_list(cls):
-        """Create submodules list with dependencies of this module"""
-        super()._create_submodules_list(
-            [
-                {"interface": "iob_s_port"},
-                {"interface": "iob_s_portmap"},
-                {"interface": "iob_wire"},
-                {"interface": "clk_en_rst_s_s_portmap"},
-                {"interface": "clk_en_rst_s_port"},
-                iob_utils,
-                iob_reg_re,
-            ]
-        )
+    IF_DISPLAY_NAME = {
+        "iob": "IOb",
+        "axil": "AXI-Lite",
+        "wb": "Wishbone",
+    }
 
-    @classmethod
-    def _setup_confs(cls):
-        super()._setup_confs(
-            [
-                # Macros
-                # Parameters
-                # IOb-bus Parameters
-                {
-                    "name": "DATA_W",
-                    "type": "P",
-                    "val": "32",
-                    "min": "NA",
-                    "max": "NA",
-                    "descr": "Data bus width",
-                },
-                {
-                    "name": "ADDR_W",
-                    "type": "P",
-                    "val": "16",
-                    "min": "NA",
-                    "max": "NA",
-                    "descr": "Address bus width",
-                },
-                # PLIC Parameters
-                {
-                    "name": "N_SOURCES",
-                    "type": "P",
-                    "val": "64",
-                    "min": "NA",
-                    "max": "64",
-                    "descr": "Number of interrupt sources",
-                },
-                {
-                    "name": "N_TARGETS",
-                    "type": "P",
-                    "val": "4",
-                    "min": "NA",
-                    "max": "8",
-                    "descr": "Number of interrupt targets",
-                },
-                {
-                    "name": "PRIORITIES",
-                    "type": "P",
-                    "val": "8",
-                    "min": "NA",
-                    "max": "8",
-                    "descr": "Number of Priority levels",
-                },
-                {
-                    "name": "MAX_PENDING_COUNT",
-                    "type": "P",
-                    "val": "8",
-                    "min": "NA",
-                    "max": "8",
-                    "descr": "Max. number of 'pending' events",
-                },
-                {
-                    "name": "HAS_THRESHOLD",
-                    "type": "P",
-                    "val": "1",
-                    "min": "NA",
-                    "max": "1",
-                    "descr": "Is 'threshold' implemented?",
-                },
-                {
-                    "name": "HAS_CONFIG_REG",
-                    "type": "P",
-                    "val": "1",
-                    "min": "NA",
-                    "max": "1",
-                    "descr": "Is the 'configuration' register implemented?",
-                },
-            ]
-        )
-
-    @classmethod
-    def _setup_ios(cls):
-        cls.ios += [
-            {"name": "iob_s_port", "descr": "CPU native interface", "ports": []},
+    attributes_dict = {
+        "generate_hw": True,
+        "description": "IObundle's Platform-Level Interrupt Controller (PLIC).",
+        "version": "0.1",
+        #
+        # Confs
+        #
+        "confs": [
+            # IOb-bus Parameters
             {
-                "name": "general",
-                "descr": "GENERAL INTERFACE SIGNALS",
-                "ports": [
-                    {
-                        "name": "clk_i",
-                        "type": "I",
-                        "n_bits": "1",
-                        "descr": "System clock input",
-                    },
-                    {
-                        "name": "arst_i",
-                        "type": "I",
-                        "n_bits": "1",
-                        "descr": "System reset, asynchronous and active high",
-                    },
-                    {
-                        "name": "cke_i",
-                        "type": "I",
-                        "n_bits": "1",
-                        "descr": "System clock enable",
-                    },
-                ],
+                "name": "DATA_W",
+                "descr": "Data bus width",
+                "type": "P",
+                "val": "32",
+                "min": "NA",
+                "max": "NA",
+            },
+            {
+                "name": "ADDR_W",
+                "descr": "Address bus width",
+                "type": "P",
+                "val": "16",
+                "min": "NA",
+                "max": "NA",
+            },
+            # PLIC Parameters
+            {
+                "name": "N_SOURCES",
+                "descr": "Number of interrupt sources",
+                "type": "P",
+                "val": "64",
+                "min": "NA",
+                "max": "64",
+            },
+            {
+                "name": "N_TARGETS",
+                "descr": "Number of interrupt targets",
+                "type": "P",
+                "val": "4",
+                "min": "NA",
+                "max": "8",
+            },
+            {
+                "name": "PRIORITIES",
+                "descr": "Number of Priority levels",
+                "type": "P",
+                "val": "8",
+                "min": "NA",
+                "max": "8",
+            },
+            {
+                "name": "MAX_PENDING_COUNT",
+                "descr": "Max. number of 'pending' events",
+                "type": "P",
+                "val": "8",
+                "min": "NA",
+                "max": "8",
+            },
+            {
+                "name": "HAS_THRESHOLD",
+                "descr": "Is 'threshold' implemented?",
+                "type": "P",
+                "val": "1",
+                "min": "NA",
+                "max": "1",
+            },
+            {
+                "name": "HAS_CONFIG_REG",
+                "descr": "Is the 'configuration' register implemented?",
+                "type": "P",
+                "val": "1",
+                "min": "NA",
+                "max": "1",
+            },
+        ],
+        #
+        # Ports
+        #
+        "ports": [
+            {
+                "name": "clk_en_rst_s",
+                "descr": "Clock, clock enable and reset",
+                "signals": {
+                    "type": "iob_clk",
+                },
             },
             {
                 "name": "plic_io",
-                "descr": "PLIC specific IO.",
-                "ports": [
-                    # {'name':'interrupt', 'type':'O', 'n_bits':'1', 'descr':'be done'},
+                "descr": "",
+                "signals": [
+                    # {'name':'interrupt_o', 'width':'1', 'descr':'be done'},
                     {
-                        "name": "src",
-                        "type": "I",
-                        "n_bits": "N_SOURCES",
+                        "name": "src_i",
                         "descr": "Interrupt sources",
+                        "width": "N_SOURCES",
                     },
                     {
-                        "name": "irq",
-                        "type": "O",
-                        "n_bits": "N_TARGETS",
+                        "name": "irq_o",
                         "descr": "Interrupt Requests",
+                        "width": "N_TARGETS",
                     },
                 ],
             },
-        ]
-
-    @classmethod
-    def _setup_regs(cls):
-        cls.regs += [
             {
-                "name": "dummy",
-                "descr": "Dummy registers to run register setup functions",
-                "regs": [
+                "name": "csrs_cbus_s",
+                "descr": f"Control and status interface, when selecting the {IF_DISPLAY_NAME[CSR_IF]} CSR interface.",
+                "signals": {
+                    "type": CSR_IF,
+                    "ADDR_W": 5,
+                    "DATA_W": 32,
+                    "STRB_W": 4,
+                },
+            },
+        ],
+        #
+        # Wires
+        #
+        "wires": [
+            {
+                "name": "internal_wires",
+                "descr": "",
+                "signals": [
+                    # IOb-bus write action
+                    {"name": "iob_write", "width": 1},
+                    {"name": "iob_we", "width": 1},
+                    {"name": "iob_re", "width": 1},
+                    # Decoded registers
+                    {"name": "el", "width": "N_SOURCES"},
+                    {"name": "ip", "width": "N_SOURCES"},
+                    {"name": "p", "width": "PRIORITY_BITS*N_SOURCES"},
+                    {"name": "ie", "width": "N_TARGETS*N_SOURCES"},
+                    {"name": "th", "width": "PRIORITY_BITS*N_TARGETS"},
+                    {"name": "id", "width": "SOURCES_BITS*N_TARGETS"},
+                    #
+                    {"name": "claim", "width": "N_TARGETS"},
+                    {"name": "complete", "width": "N_TARGETS"},
+                ],
+            },
+        ],
+        #
+        # Subblocks
+        #
+        "subblocks": [
+            {  # Currently used for docs only
+                "core_name": "iob_csrs",
+                "instantiate": False,
+                "instance_name": "csrs",
+                "instance_description": "Control/Status Registers",
+                "csrs": [
                     {
-                        "name": "DUMMY",
-                        "type": "R",
+                        "name": "dummy_reg",
+                        "descr": "Dummy register for demo",
+                        "type": "NOAUTO",
+                        "mode": "R",
                         "n_bits": 1,
                         "rst_val": 0,
-                        "addr": 0x8000,
                         "log2n_items": 0,
-                        "autologic": False,
-                        "descr": "Dummy Register",
+                        #"addr": 0x8000,
                     },
                 ],
-            }
-        ]
+                "csr_if": "iob",
+                "connect": {
+                    "clk_en_rst_s": "clk_en_rst_s",
+                    # 'control_if_m' port connected automatically
+                },
+            },
+        ],
+        "sw_modules": [
+            # Software modules
+            {
+                "core_name": "iob_linux_device_drivers",
+            },
+        ],
+        #
+        # Snippets
+        #
+        "snippets": [
+            {
+                "verilog_code": """
+   // Local parameters
+   localparam SOURCES_BITS = $clog2(N_SOURCES + 1);  //0=reserved
+   localparam PRIORITY_BITS = $clog2(PRIORITIES);
 
-    @classmethod
-    def _setup_block_groups(cls):
-        cls.block_groups += []
+   /** APB Read/Write */
+   assign iob_write = |iob_wstrb_i;
+   assign iob_re    = iob_avalid_i & ~iob_write;
+   assign iob_we    = iob_avalid_i & iob_write;
+
+
+   // Module intanciation
+
+   /** Hookup Dynamic Register block
+   */
+   plic_dynamic_registers #(
+      //Bus Interface Parameters
+      .ADDR_SIZE(ADDR_W),
+      .DATA_SIZE(DATA_W),
+
+      //PLIC Parameters
+      .SOURCES          (N_SOURCES),
+      .TARGETS          (N_TARGETS),
+      .PRIORITIES       (PRIORITIES),
+      .MAX_PENDING_COUNT(MAX_PENDING_COUNT),
+      .HAS_THRESHOLD    (HAS_THRESHOLD),
+      .HAS_CONFIG_REG   (HAS_CONFIG_REG)
+   ) dyn_register_inst (
+      .rst_n(~arst_i),  //Active low asynchronous reset
+      .clk  (clk_i),    //System clock
+
+      .we   (iob_we),       //write cycle
+      .re   (iob_re),       //read cycle
+      .be   (iob_wstrb_i),  //STRB=byte-enables
+      .waddr(iob_addr_i),   //write address
+      .raddr(iob_addr_i),   //read address
+      .wdata(iob_wdata_i),  //write data
+      .rdata(iob_rdata_o),  //read data
+
+      .el      (el),       //Edge/Level
+      .ip      (ip),       //Interrupt Pending
+      .ie      (ie),       //Interrupt Enable
+      .p       (p),        //Priority
+      .th      (th),       //Priority Threshold
+      .id      (id),       //Interrupt ID
+      .claim   (claim),    //Interrupt Claim
+      .complete(complete)  //Interrupt Complete
+   );
+
+
+   /** Hookup PLIC Core
+   */
+   plic_core #(
+      .SOURCES          (N_SOURCES),
+      .TARGETS          (N_TARGETS),
+      .PRIORITIES       (PRIORITIES),
+      .MAX_PENDING_COUNT(MAX_PENDING_COUNT)
+   ) plic_core_inst (
+      .rst_n(~arst_i),
+      .clk  (clk_i),
+
+      .src      (src),
+      .el       (el),
+      .ip       (ip),
+      .ie       (ie),
+      .ipriority(p),
+      .threshold(th),
+
+      .ireq    (irq),
+      .id      (id),
+      .claim   (claim),
+      .complete(complete)
+   );
+
+""",
+            },
+        ],
+    }
+    #
+    # Combs
+    #
+    attributes_dict["comb"] = {
+        "code": """
+    // Interface Registers
+
+    iob_rvalid_o_en = ~iob_write;
+    iob_rvalid_o_nxt = iob_avalid_i;
+
+    // Ready signal, is always 1 since the read and write to the PLIC only take one clock cycle.
+    iob_ready_o = 1'b1;
+
+"""
+    }
+
+    return attributes_dict
